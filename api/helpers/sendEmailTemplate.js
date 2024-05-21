d2523cd9bce8bd2190c2f3ed242bddf0a7823fb7
Pranav <pranav.2002tis@gmail.com>
2024-05-21 23:34:45 +0530

    Added AWS SES Config

--- api/helpers/sendEmailTemplate.js
+++ api/helpers/sendEmailTemplate.js
@@ -1,35 +1,43 @@
-import aws from 'aws-sdk'
-import { CustomError } from './expressUtils'
+import aws from "aws-sdk";
+import { CustomError } from "./expressUtils";
 
-aws.config.update({ region: 'ap-south-1' })
+const SES_CONFIG = {
+  accessKeyId: "AKIAUDE2IDGW6QOSWU74",
+  secretAccessKey: "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",
+  region: "ap-south-1",
+};
 
-export async function sendMail (Source, recipientEmail, subject, mailBody, BccAddresses = []) {
+export async function sendMail(
+  Source,
+  recipientEmail,
+  subject,
+  mailBody,
+  BccAddresses = []
+) {
   try {
     const params = {
       Source,
       Destination: {
-        ToAddresses: [
-          recipientEmail
-        ],
-        BccAddresses: BccAddresses
+        ToAddresses: [recipientEmail],
+        BccAddresses: BccAddresses,
       },
       ReplyToAddresses: [],
       Message: {
         Body: {
           Html: {
-            Charset: 'UTF-8',
-            Data: mailBody
-          }
+            Charset: "UTF-8",
+            Data: mailBody,
+          },
         },
         Subject: {
-          Charset: 'UTF-8',
-          Data: subject
-        }
-      }
-    }
-    const mailSent = new aws.SES().sendEmail(params).promise()
-    return mailSent
+          Charset: "UTF-8",
+          Data: subject,
+        },
+      },
+    };
+    const mailSent = new aws.SES(SES_CONFIG).sendEmail(params).promise();
+    return mailSent;
   } catch (error) {
-    throw new CustomError(error)
+    throw new CustomError(error);
   }
 }
